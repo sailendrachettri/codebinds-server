d257f8848a5283557f4378e0366b8eac443df695
sailendrachettri <sailendra9083@gmail.com>
2024-04-09 22:59:43 +0530

    .env added

--- routes/auth.js
+++ routes/auth.js
@@ -13,8 +13,8 @@ const JWT_SECRET_KEY = process.env.REACT_APP_JWT_SECRET_KEY
 // ROUTE 1: Create an api for registration page using post requrest : "api/auth/register"
 router.post('/register', [
     body("username", "Username length should be more than 3 characters.").isLength({ min: 5 }),
-    body("phone", "Invalid phone number").isLength({min: 10, max: 10}),
-    body("password", "Weak Password, make it stronger by using Alphanumberic and symbols").isLength({min: 6})
+    body("phone", "Invalid phone number").isLength({ min: 10, max: 10 }),
+    body("password", "Weak Password, make it stronger by using Alphanumberic and symbols").isLength({ min: 6 })
 ], async (req, res) => {
     let success = false;
 
@@ -60,26 +60,26 @@ router.post('/register', [
     }
 })
 
-// ROUTE 3: Create an api for login page using post requrest : "api/auth/login"
+// ROUTE 2: Create an api for login page using post requrest : "api/auth/login"
 router.post('/login', [
-    body("username", "Invalid credentials").isLength({min: 3}),
-    body("password", "Invalid credentials").isLength({min: 3})
-] ,async (req, res) => {
+    body("username", "Invalid credentials").isLength({ min: 3 }),
+    body("password", "Invalid credentials").isLength({ min: 3 })
+], async (req, res) => {
     let success = false;
 
     try {
-         // express validation
-         const error = validationResult(req);
-         if (!(error.isEmpty())) {
-             return res.status(401).json({ success, message: error.array()[0]["msg"] });
+        // express validation
+        const error = validationResult(req);
+        if (!(error.isEmpty())) {
+            return res.status(401).json({ success, message: error.array()[0]["msg"] });
         }
-        
+
         let user = await Users.findOne({ username: req.body.username });
 
         // if user doesn't exist 
         if (!user)
             return res.status(404).json({ success, message: "User doesn't exist. Try registration first!" });
-            
+
         // compare user entered password with hash password if it false the return
         if (!(bcryptjs.compareSync(req.body.password, user.password)))
             return res.status(400).json({ success, message: "Invalid username or password" });
@@ -92,12 +92,13 @@ router.post('/login', [
         }
         const auth_token = jwt.sign(data, JWT_SECRET_KEY);
 
-        
+
 
         success = true;
         res.status(200).json({ success, message: "Logged In successful!", auth_token, username: user.username });
 
     } catch (error) {
+        console.log("hereee");
         res.status(500).json({ success, message: "Internal server error" });
     }
 })
