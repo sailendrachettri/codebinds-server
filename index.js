4a6f29e64e0b869fa518fd0ae2e77588c99b9a83
sailendrachettri <sailendra9083@gmail.com>
2024-04-11 00:00:44 +0530

    model is ready and it saves data

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 // Available routes
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/post', require('./routes/post'));
 
 
 app.listen(PORT, ()=>{
---
+++ routes/post.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' })
+const fs = require('fs');
+const Post = require('../models/PostSchema');
+const JWT_SECRET_KEY = process.env.REACT_APP_JWT_SECRET_KEY
+const jwt = require("jsonwebtoken");
+
+router.post('/create', uploadMiddleware.single('file'), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+
+
+    // get the user data and send to client
+    const { auth_token } = req.cookies;
+    jwt.verify(auth_token, JWT_SECRET_KEY, async(err, info) => {
+        if (err)
+            return res.status(500).json({ message: "Internal server error" })
+
+        const { title, summary, content } = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id
+        })
+
+        res.json(postDoc)
+    });
+})
+
+router.get('/fetchpost', async (req, res) => {
+    const posts = await Post.find();
+    res.json(posts)
+})
+
+module.exports = router
