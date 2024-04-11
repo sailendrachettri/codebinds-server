a6187d8e23c36e5164d9b961288a601abb3d5afb
sailendrachettri <sailendra9083@gmail.com>
2024-04-11 11:58:20 +0530

    endpoint added to edit

--- routes/post.js
+++ routes/post.js
@@ -18,7 +18,7 @@ router.post('/create', uploadMiddleware.single('file'), async (req, res) => {
 
     // get the user data and send to client
     const { auth_token } = req.cookies;
-    jwt.verify(auth_token, JWT_SECRET_KEY, {}, async(err, info) => {
+    jwt.verify(auth_token, JWT_SECRET_KEY, {}, async (err, info) => {
         // if (err) throw err;
         if (err)
             return res.status(500).json({ message: "Internal server error" })
@@ -39,17 +39,59 @@ router.post('/create', uploadMiddleware.single('file'), async (req, res) => {
 // ROUTE 2: FETCH ALL THE POSTS AS A CARD IN HOMEPAGE
 router.get('/fetchpost', async (req, res) => {
     const posts = await Post.find()
-    .populate('author', ['username'])
-    .sort({createdAt: -1})
-    .limit(20)
+        .populate('author', ['username'])
+        .sort({ createdAt: -1 })
+        .limit(20)
     res.json(posts)
 })
 
 // ROUTE 3: NOW  FETCH CARDS AS A SINGLE PAGE
-router.get('/card/:id', async(req, res)=>{
-    const {id} = req.params;
+router.get('/card/:id', async (req, res) => {
+    const { id } = req.params;
     const postDoc = await Post.findById(id).populate('author', 'username');
     res.json(postDoc)
+})
+
+// ROUTE 4: EDIT THE EXISTING POST
+router.put('/edit', uploadMiddleware.single('file'), async (req, res) => {
+
+    let newPath = null;
+
+    if (req.file) {
+        const { originalname, path } = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+    }
+
+    // get the user data and send to client
+    const { auth_token } = req.cookies;
+    jwt.verify(auth_token, JWT_SECRET_KEY, {}, async (err, info) => {
+        // if (err) throw err;
+        if (err)
+            return res.status(500).json({ message: "Internal server error" })
+
+        const {id, title, summary, content } = req.body;
+        const postDoc = await Post.findById(id);
+
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        
+        if(!isAuthor){
+            return res.status(400).json("you are not an author");            
+        }
+
+        await postDoc.updateOne({
+            title,
+            summary,
+            content,
+            cover: newPath ? newPath : postDoc.cover
+        });
+
+        res.json(postDoc);
+
+    });
+
 })
 
 module.exports = router
